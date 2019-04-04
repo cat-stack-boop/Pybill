a611d56b7945f741fedc41fbc61e62a8f1729dd8
4chin <lloydofarabia@gmail.com>
2019-04-04 23:21:42 +1100

    Update SEA.py

--- SEA.py
+++ SEA.py
@@ -1,30 +1,20 @@
 print('------------------------------------------------\n\
 Canberra Snowy hydro Energy \n\
-------------------------------------------------')    # New line formating 
+------------------------------------------------')
 
-
-previous = input("Enter the previous reading :")       # user enters a number which is saved as a variable 
+previous = input("Enter the previous reading :")
 
 current = input("Enter the current reading :") 
 
-
-
 difference = current - previous                  
-
-
-
 
 if (difference <= 1000):                                 
 		amount = difference * 0.15
 elif (difference >1000):
-	amount = (1000 * 0.15) + (difference - 1000) * 0.25            # need to cut total amount into different tariff brackets 
-
-
+	amount = (1000 * 0.15) + (difference - 1000) * 0.25           
 
 print('Cheers Mate!')
 
 print('The bill amount is:$') + str(amount) # converting an int to a string
-
-
 
 print('Till next time.')
